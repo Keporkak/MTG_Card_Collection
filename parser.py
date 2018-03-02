9370ea515a8edd3354dd7b318a64292add6b7aeb
Keporkak <k3porkak@gmail.com>
2018-03-02 20:34:45 +0100

    +price and rarity

--- parser.py
+++ parser.py
@@ -4,7 +4,7 @@ from pprint import pprint as pp
 
 
 def series_for_card(card_name):
-    card_search_url = 'https://www.cardkingdom.com/catalog/search?search=header&filter%5Bname%5D={}'
+    card_search_url = 'https://www.cardkingdom.com/catalog/search?filter%5Bipp%5D=60&filter%5Bsort%5D=name&filter%5Bname%5D={}'
     search_result = requests.get(card_search_url.format(card_name))
     d = pq(search_result.content)
     results = d('div.productListRow div.mainListing div.productCardWrapper')
@@ -15,13 +15,13 @@ def series_for_card(card_name):
     for card in results.items():
         title = list(card.find('div.itemContentWrapper .productDetailTitle a').items())[0].text().strip()
         description = list(card.find('div.itemContentWrapper div.productDetailSet').items())[0].text().strip()
-        description = description[:-4]
+        price = list(card.find('div.itemContentWrapper div.addToCartWrapper div.amtAndPrice .stylePrice').items())[0].text().strip()
+        rarity = description[-2:-1]
 
         if title not in series:
             series[title] = []
 
-        series[title].append(description)
-#        editions.append(description)
+        series[title].append([description[:-4], rarity, price])
 
     return series
 

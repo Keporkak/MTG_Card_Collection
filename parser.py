ce015062601e3975b452e7176d1ad800e2438962
Keporkak <k3porkak@gmail.com>
2018-03-04 21:30:26 +0100

    polished

--- parser.py
+++ parser.py
@@ -29,8 +29,14 @@ def series_for_card(card_name):
 def specfic_card_cost(card_name, edition):
     price = 'NOT_FOUND'
 
+    with open('edColl.json', 'r') as jsoun:
+        editions = json.loads(jsoun.read())
+        jsoun.close()
+
+    selected = editions[edition]
+
     card_url = 'https://www.cardkingdom.com/catalog/view/?filter%5Bsort%5D=name&filter%5Bsearch%5D=mtg_advanced&filter%5Btab%5D=mtg_foil&filter%5Bname%5D={}&filter%5Bcategory_id%5D={}&filter%5Bmulti%5D%5B0%5D=1&filter%5Btype_mode%5D=any&filter%5Btype_key%5D=&filter%5Bpow1%5D=&filter%5Bpow2%5D=&filter%5Btuf1%5D=&filter%5Btuf2%5D=&filter%5Bconcast1%5D=&filter%5Bconcast2%5D=&filter%5Bprice_op%5D=&filter%5Bprice%5D=&filter%5Bkey_text1%5D=&filter%5Bmanaprod_select%5D=any&filter[tab]=mtg_card'
-    search_result = requests.get(card_url.format(card_name, edition))
+    search_result = requests.get(card_url.format(card_name, selected))
     d = pq(search_result.content)
     results = d('div.productListRow div.mainListing div.productCardWrapper')
 
@@ -40,6 +46,8 @@ def specfic_card_cost(card_name, edition):
     return price
 
 def load_editions():
+    print ('Loading editions... ', end='', flush=True)
+
     search_url = 'https://www.cardkingdom.com/catalog/search?filter%5Bipp%5D=60&filter%5Bsort%5D=name&filter%5Bname%5D=Search'
     search_result = requests.get(search_url)
     d = pq(search_result.content)
@@ -54,20 +62,21 @@ def load_editions():
         json.dump(editions, jsoun)
         jsoun.close()
 
-    return 'DONE'
+    print('DONE')
 
 ## ACTUAL PROGRAM ##
 
 load_editions()
 while True:
     card_name = input('Card name: ')
+    edition = input('Edition: ')
 
     with open('collection.json', 'r') as collection:
         file_content = collection.read()
         json_content = json.loads(file_content)
         collection.close()
 
-    series = specfic_card_cost(card_name, 2575)
+    series = specfic_card_cost(card_name, edition)
     #json_content.update(series)
 
     with open('collection.json', 'w') as collection:
